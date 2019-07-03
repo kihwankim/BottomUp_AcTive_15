c20ee3b9e168fe9b0b9c7b55695ae62ebc88df05
kihwankim <clack2933@gmail.com>
2019-07-03 21:39:03 +0900

    change layout

--- BottomUp_UI/src/components/main_layout/main_layout.js
+++ BottomUp_UI/src/components/main_layout/main_layout.js
@@ -1,38 +1,45 @@
 import React, {Component} from 'react';
 
-class MainLayout extends Component{
-  constructor(props){
-    super(props);
-  }
-
-  createTable = () => {
-    let table = [];
-    // Outer loop to create parent
-    for (let i = 0; i < parseInt(this.props.row); i++) {
-      let children = [];
-      //Inner loop to create children
-      for (let j = 0; j < parseInt(this.props.col); j++) {
-        children.push(<td>{`Column ${j + 1}`}</td>);
-      }
-      //Create the parent and add the children
-      table.push(<tr>{ children }</tr>);
+class MainLayout extends Component {
+    constructor(props) {
+        super(props);
     }
-    return table
-  }
 
-  render() {
-      if(!this.props.row || this.props.row == 0
-        || !this.props.col || this.props.col == 0){
-          return <div> please wait...</div>
+    writeContent(data) {
+        console.log(data);
+    }
+
+    createTable = () => {
+        let table = [];
+        // Outer loop to create parent
+
+        for (let i = 0; i < parseInt(this.props.row); i++) {
+            let children = [];
+            //Inner loop to create children
+            for (let j = 0; j < parseInt(this.props.col); j++) {
+                children.push(<td onClick={event => this.writeContent(event.target)}></td>);
+            }
+            //Create the parent and add the children
+            table.push(<tr>{children}</tr>);
+        }
+        return table;
+    }
+
+    render() {
+        if (!this.props.row || this.props.row == 0
+            || !this.props.col || this.props.col == 0) {
+            console.log("row is not" + this.props.row)
+            return <div> please wait...</div>
         }
 
         return (
-          <div>
-            <table className="main-layout">
-              { this.createTable() }
-            </table>
-          </div>
+            <div>
+                <table className="main-layout">
+                    {this.createTable()}
+                </table>
+            </div>
         );
-      }
     }
-    export default MainLayout;
+}
+
+export default MainLayout;
