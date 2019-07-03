179e1f66cbd6b390caeb6328dc281c2802dba3e7
kihwankim <clack2933@gmail.com>
2019-07-03 21:30:20 +0900

    change thing

--- BottomUp_UI/src/components/main_layout/main_layout.js
+++ BottomUp_UI/src/components/main_layout/main_layout.js
@@ -1,37 +1,38 @@
 import React, {Component} from 'react';
 
 class MainLayout extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            row: 5,
-            col: 3
-        };
-    }
+  constructor(props){
+    super(props);
+  }
 
-    createTable = () => {
-        let table = []
-        // Outer loop to create parent
-        for (let i = 0; i < this.state.row; i++) {
-            let children = []
-            //Inner loop to create children
-            for (let j = 0; j < this.state.col; j++) {
-                children.push(<td>{`Column ${j + 1}`}</td>)
-            }
-            //Create the parent and add the children
-            table.push(<tr>{children}</tr>)
-        }
-        return table
+  createTable = () => {
+    let table = [];
+    // Outer loop to create parent
+    for (let i = 0; i < parseInt(this.props.row); i++) {
+      let children = [];
+      //Inner loop to create children
+      for (let j = 0; j < parseInt(this.props.col); j++) {
+        children.push(<td>{`Column ${j + 1}`}</td>);
+      }
+      //Create the parent and add the children
+      table.push(<tr>{ children }</tr>);
     }
+    return table
+  }
 
-    render() {
-        return (
-            <div>
-                <table className="main-layout">
-                    {this.createTable()}
-                </table>
-            </div>
-        );
+  render() {
+    if(!this.props.row || this.props.row == 0
+      || !this.props.col || this.props.col == 0){
+        return <div> please wait...</div>
+      }
+
+      return (
+        <div>
+          <table className="main-layout">
+            { this.createTable() }
+          </table>
+        </div>
+      );
     }
-}
-export default MainLayout;
+  }
+  export default MainLayout;
