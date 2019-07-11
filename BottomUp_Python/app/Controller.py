a31d59ed8f080e20a9532cb7878cb8df71a464f1
kihwankim <clack2933@gmail.com>
2019-07-11 19:44:15 +0900

    change rep name and modeling

---
+++ BottomUp_Python/app/Controller.py
@@ -0,0 +1,13 @@
+from model.Connect import Connect
+
+
+class Controller:
+    def __init__(self):
+        self.connect = Connect()
+
+    def run(self):
+        print(self.connect.get_data());
+
+
+controller = Controller()
+controller.run()
