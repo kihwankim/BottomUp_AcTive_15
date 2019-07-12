edf2b3b6e746a66e6009f4c03cf07c3a68a0c8a7
kihwankim <clack2933@gmail.com>
2019-07-12 11:56:29 +0900

    add classes

--- BottomUp_Python/app/Controller.py
+++ BottomUp_Python/app/Controller.py
@@ -1,12 +1,14 @@
-from model.Connect import Connect
+from connectDB.Connect import Connect
+from graph.Graph import Graph
 
 
-class Controller:
+class Controller(object):
     def __init__(self):
         self.connect = Connect()
 
     def run(self):
-        print(self.connect.get_data())
+        tables = self.connect.get_data()
+        self.graph = Graph(tables)  # path 구하는 cㅣass 생성
 
 
 controller = Controller()
---
+++ BottomUp_Python/makePath/MakePath.py
@@ -0,0 +1,7 @@
+class MakePath(object):
+    def __init__(self, graph):
+        self.graph = graph
+
+    def make_paht(self):
+        # main 알고리즘 부분
+        pass
