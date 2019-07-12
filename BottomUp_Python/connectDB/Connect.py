b0d714d01074747ec9b8156d1ca668373632a148
kihwankim <clack2933@gmail.com>
2019-07-12 12:10:50 +0900

    delete model pakage and change a little code

--- BottomUp_Python/connectDB/Connect.py
+++ BottomUp_Python/connectDB/Connect.py
@@ -10,14 +10,11 @@ class Connect(object):
     def get_data(self):
 
         result = self.firebase.get('/bottomup', None)
-
         tables = [[] for _ in range(len(result))]
-
-        insertIndex = 0
 
         for key, value in result.items():
             for tableInfo in value:
-                if ('height' in tableInfo.keys()):
+                if 'height' in tableInfo.keys():
                     insetIndex = tableInfo['height'] - 1
                 else:
                     tables[insetIndex].append(Pi(tableInfo))
