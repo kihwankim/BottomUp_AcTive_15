eb80c00661cb459a8f2b91fc5cc4ca4928f81c42
zzz845zz <zzz845zz@gmail.com>
2019-07-13 02:35:46 +0900

    change NetworkController : add safes array

--- BottomUp_Python/network/networkController.py
+++ BottomUp_Python/network/networkController.py
@@ -1,3 +1,3 @@
 class NetworkController:
     def __init__(self, tables):
-        self.tables = tables
+        self.safes = [1]*(len(tables[0])+1)   # 각 파이별로 안전한지 나타냄.  [1]=1 : 1번 파이 safe,  [2]=0 : 2번 파이 unsafe
