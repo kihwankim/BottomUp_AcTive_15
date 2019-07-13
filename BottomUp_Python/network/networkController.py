dc44a672f5ebda7033823dc7e9d9179bff62dddb
zzz845zz <zzz845zz@gmail.com>
2019-07-14 05:56:58 +0900

    big update network controller

--- BottomUp_Python/network/networkController.py
+++ BottomUp_Python/network/networkController.py
@@ -64,9 +64,10 @@ class NetworkController:
             # 새로운 클라이언트와 연결
             client_socket, addr = self.server_socket.accept()
 
+            pi_num = int.from_bytes(client_socket.recv(1), byteorder='big')
             # 클라이언트에게서 수신할 객체, 클라이언트에게 송신할 객체 생성
-            new_receiver = ReceiverSocket(client_socket, addr)
-            new_sender = SenderSocket(client_socket, addr)
+            new_receiver = ReceiverSocket(client_socket, pi_num)
+            new_sender = SenderSocket(client_socket, pi_num)
 
             # 주기적으로 수신 시작
             t_receive = Thread(target=self.action_receive, args=(new_receiver,))
@@ -77,38 +78,58 @@ class NetworkController:
             self.senders.append(new_sender)
 
     # 주기적으로 수신받아서 self.safes 업데이트 (파이 하나당 스레드 하나로 사용)
-    def action_receive(self, ReceiverSocket):
-        data = ReceiverSocket.receive_data()
-        if data=='':
-            ReceiverSocket.close()
+    def action_receive(self, ReceiverSock):
+        # 첫 수신 처리
+        pi_num, message = ReceiverSock.receive_data()
+        print("첫 수신",pi_num,message) # debug
+        if message == 'disconnected':
+            ReceiverSock.close()
+            del ReceiverSock
             return
         # 큐에 아이템을 넣어, 메인 컨트롤러에서 emergency 상황을 인지하도록 함
-        elif data=='emergency':
+        elif message=='emergency':
             self.queue.put('emergency')
         # emergency 상황을 인지한 파이는, 서버에게 [파이번호, safe or unsafe] 데이터를 계속해서 송신
+        elif pi_num != ReceiverSock.get_pi_num():
+            print("[수신 에러. PI NUM 오류] 소켓 PI번호:%d, 수신받은 PI번호:%d" %(pi_num, ReceiverSock.get_pi_num()))
         else:
-            self.safes[ord(data[0])] = ord(data[1])
+            self.safes[pi_num] = message
 
-        time.sleep(0.1)
+        # 두 번째 수신부터 반복
         while True:
-            data = ReceiverSocket.receive_data()
-            self.safes[ord(data[0])] = ord(data[1])
             time.sleep(0.1)
+            pi_num, message = ReceiverSock.receive_data()
+            print("수신",pi_num,message) # debug
+            if message == 'disconnected':
+                ReceiverSock.close()
+                del ReceiverSock
+                return
+            self.safes[pi_num] = message
     
     # 각각의 파이에게 송신을 반복. (스레드 하나가 사용) 
     def action_send(self):
-        # 각각의 파이에게 0을 보내서 emergency 상황임을 알림
+        # 각각의 파이에게 255를 보내서 emergency 상황임을 알림
         for SenderSocket in self.senders:
-            SenderSocket.send_data(0)
+            SenderSocket.send_data(255)
 
+        ### TEST ###
+        #t = Thread(target=self.test_put_queue)
+        #t.start()
+        ############
         while True:
             list_path = self.queue.get()
+            if list_path == 'emergency':
+                continue
             for path in list_path:
-                # path[0] : 파이 번호, path[1] : 가리킬 방향
+                # path[0] : 파이 번호, path[1] : 가리킬 방향(숫자 0~254로 표시)
                 # 해당 번호의 파이에게 가리킬 방향을 송신
                 self.senders[path[0]].send_data(path[1])
 
     ### TEST ###
+    def test_put_queue(self):
+        while True:
+            test_data = int(input("송신 입력 : "))
+            self.queue.put([[0, test_data]])
 
     def test_run_server(self):
         self.t_server = Thread(target=self.test_accept_connect)
