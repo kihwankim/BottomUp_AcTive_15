6dbc868a37acfa3a793df13f8688ae1175a7bca2
zzz845zz <zzz845zz@gmail.com>
2019-07-19 05:26:03 +0900

    Big Update All of all to easy Network Controll

--- BottomUp_Python/network/networkController.py
+++ BottomUp_Python/network/networkController.py
@@ -9,6 +9,8 @@ from network.send.Sender import Sender
 
 class NetworkController:
     def __init__(self, pi_datas, building_height, ip, port):
+        self.ip = ip
+        self.port = port
         self.capacity = len(pi_datas) # 정점(파이) 수용량
 
         # 각 층별로, 파이가 안전한지 나타냄. 값의 의미는 (-1:미연결, 0:위험, 1:안전)
@@ -25,68 +27,52 @@ class NetworkController:
             self.safes_height[height] = {int(x.piNumber):-1 for x in pi_datas if x.height==height}
 
         self.size_connection = 0
-        
-        # 서버 소켓 설정
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((ip, port))
-        self.server_socket.listen(0)
 
          # 작업 Queue.
          # 큐에서 아이템을 꺼낼 때, 비어있으면 꺼낼때까지 그대로 멈춘다. 이 특성을 이용해 작업 순서 조율
-        self.queue = Queue()
+        self.q_from_Receiver = Queue()
 
     def get_safes_height(self):
         return self.safes_height
 
-    def get_queue_item(self):
-        return self.queue.get()
+    def get_status(self):
+        return self.q_from_Receiver.get()
 
-    def put_queue(self, data):
-        self.queue.put(data)
+#-----------------------------------#
+    def start_accpet(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.ip, self.port))
+        self.server_socket.listen(0)
 
-    # 서버의 통신 시작
-    def run_server(self):
-        # 접속요청 처리를 담당할 스레드
-        self.t_server = Thread(target=self.accept_connect)
-        self.t_server.daemon = True
-        self.t_server.start()
+        try:
+            while True:
+                # 새로운 클라이언트와 연결
+                client_socket, addr = self.server_socket.accept()
+                
+                t_judge_accpet = Thread(target=self.judge_connect, args=(client_socket, addr))
+                # 데몬 True : 부모 스레드가 종료되면 자신도 종료
+                t_judge_accpet.daemon = True
+                t_judge_accpet.start()
+        except OSError:
+            print("server stopped")
 
-        # 연결된 클라이언트들에게 송신을 담당할 스레드
-        self.t_run_send = Thread(target=self.run_send_while_emergency)
-        self.t_run_send.daemon = True
-        self.t_run_send.start()
-        
-        #####
-        self.t_server.join()
 
-    def run_send_while_emergency(self):
-        while True:
-            # 재난상황 발생시 송신 시작
-            if self.queue.get()==('emergency'):
-                # 관리자에 의한 상황종료를 담당할 스레드
-                t_stop = Thread(target=self.stop_emergency_by_admin)
-                t_stop.start()
-                self.action_send_emergency()
-            else:
-                print("run_send_while_emergency() else문")
-    
-    # 상황종료는 관리자가 판단
-    def stop_emergency_by_admin(self):
-        self.wait_YES_with_query("Stop Emergency?")
 
-        # 송신스레드가 상황종료를 알아차리도록, 큐로 전달
-        self.queue.put('stop emergency')
+    def stop_accept(self):
+        self.server_socket.close()
+        del self.server_socket
 
-    # 클라이언트의 접속을 무한히 처리. 클라이언트 연결시, 해당 클라이언트에게서 주기적으로 수신하는 스레드 생성
-    def accept_connect(self):
-        while True:
-            # 새로운 클라이언트와 연결
-            client_socket, addr = self.server_socket.accept()
-            
-            t_judge_accpet = Thread(target=self.judge_connect, args=(client_socket, addr))
-            # 부모가 종료되면 데몬스레드도 모두 종료(self.t_server 스레드가 종료되면, 수신 스레드도 모두 종료)
-            t_judge_accpet.daemon = True
-            t_judge_accpet.start()
+    def start_checking(self):
+        self.SendManager.send_All_start_checking()
+
+    def stop_checking(self):
+        self.SendManager.send_All_stop_checking()
+
+    def start_emergency(self):
+        self.SendManager.send_All_start_emergency()
+
+    def stop_emergency(self):
+        self.SendManager.send_All_stop_checking()
 
     def judge_connect(self, client_socket, addr):
         try:
@@ -99,11 +85,11 @@ class NetworkController:
                     
             # 클라이언트에게서 수신할 객체, 클라이언트에게 송신할 객체 생성
             new_sender = Sender(client_socket, pi_floor, pi_num)
-            new_receiver = Receiver(client_socket, pi_floor, pi_num, self.safes_height, self.queue)
+            new_receiver = Receiver(client_socket, pi_floor, pi_num, self.safes_height, self.q_from_Receiver)
 
             # 송신, 수신
             self.SendManager.add_sender(new_sender)
-            message_return = new_receiver.action_receive()
+            message_return = new_receiver.run()
             
             # 연결 끊킨 파이 제거
             if message_return == 'delete this connection':
@@ -144,7 +130,10 @@ class NetworkController:
                 pass
             finally:
                 print("%s의 번호 요청. %s층 %s번 할당 불가. 접속 거부" %(sock.getpeername(), pi_floor, pi_num))
-    
+  
+    def send_All_path(self, list_path):
+        self.SendManager.send_All_path(list_path)
+
     # 현재 빌딩의 남는 파이 자리를 문자열로 리턴
     def string_extra_seat(self):
         ret = str()
@@ -154,56 +143,8 @@ class NetworkController:
             height_remain_pi = [key for ix, key in enumerate(pi_dict) if pi_dict[key]==-1]
             ret += ("%s층 : %s\n" %(height, height_remain_pi))
         return ret     
-    
-    # 상황발생시, 파이에게 송신하는 함수. (스레드 하나가 사용) 
-    def action_send_emergency(self):
-        # 각각의 파이에게 255를 보내서 emergency 상황임을 알림
-        self.SendManager.send_All_start_emergency()
 
-        ### TEST ###
-        #t = Thread(target=self.test_put_queue)
-        #t.start()
-        ############
-        
-        while True:
-            try:
-                list_path = self.queue.get()
-                if list_path == 'emergency':
-                    continue
-                if list_path == 'stop emergency':
-                    break
-                for path in list_path:
-                    # path[0]:층, path[0]:파이 번호, path[1]:가리킬 방향(숫자 0~100로 표시)
-                    # 해당 번호의 파이에게 가리킬 방향을 송신
-                    self.SendManager.send_message(path[0], path[1], path[2])
-            except KeyError as e:
-                print("[송신 에러] 존재하지 않는 파이(%s)에게 송신시도\n" %(str(e)))
-            except OSError:
-                print("[송신 예외처리][action_send] 닫힌 소켓에게 송신 시도")
-            except OverflowError:
-                print("[송신 예외처리] 1바이트(0~255) 범위 메세지만 송신 가능")
-
-        # 모두에게 '상황종료' 송신
-        self.SendManager.send_All_stop_checking()
-
-        # 관리자 입력 대기
-        self.wait_YES_with_query("상황체크 재시작?")
-
-        # 모두에게 '상황체크 시작' 송신
-        self.SendManager.send_All_start_checking()
-    
-    # query(질문)에 대한 관리자의 YES 입력 대기
-    def wait_YES_with_query(self, query):
-        while True:
-            try:
-                order = input(str(query)+"? (YES or NO) :")
-                if order == 'YES':
-                    return 'YES'
-            except ValueError:
-                print("[입력 에러]")
-                pass
-    
-    # 실행도중 연결 끊킨 파이 처리
+    # 연결 끊킨 파이 처리
     def delete_disconnected_client(self, client_socket, floor, pi_num):
         try:
             # 소켓 close
@@ -217,3 +158,29 @@ class NetworkController:
         except NameError:
             #print("[delete_disconnected_socket] 이미 삭제된 객체")
             pass
+
+#------------------------------------#
+    
+    def start_server_by_admin(self):
+        self.wait_YES_with_query("Start Server?")
+
+        # 서버 소켓 설정
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.ip, self.port))
+        self.server_socket.listen(0)
+
+
+    def stop_server_by_admin(self):
+        self.wait_YES_with_query("Stop Server?")
+        self.server_socket.close()
+    
+    # query(질문)에 대한 관리자의 YES 입력 대기
+    def wait_YES_with_query(self, query):
+        while True:
+            try:
+                order = input(str(query)+"? (YES or NO) :")
+                if order == 'YES':
+                    return 'YES'
+            except ValueError:
+                print("[입력 에러]")
+                pass
