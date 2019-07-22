efebb904804a0623802b6bd677e4cdbcfeb2cff4
JiHunBae <qowlgns304@gmail.com>
2019-07-23 05:41:20 +0900

    Added comment about code

--- BottomUp_Python_PI/sensor/Temperature_Check_DHT11/temp_Check.py
+++ BottomUp_Python_PI/sensor/Temperature_Check_DHT11/temp_Check.py
@@ -1,5 +1,7 @@
 import Adafruit_DHT
 def check_Temperature():
+    # 일정 온도가 넘어가지 않을 시 True를 반환, 온도 넘어가면 False를 반환
+    # Normal Situation 에서는 True, Emergency Situation 에서는 False를 반환
     sensor = Adafruit_DHT.DHT11
     pin = 17
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
