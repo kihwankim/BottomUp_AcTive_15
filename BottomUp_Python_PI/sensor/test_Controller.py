fc721c3356a77992ff0f53469dc26820af7b5c7f
JiHun Bae <qowlgns304@gmail.com>
2019-08-09 03:04:57 +0900

    Modify some functions and codes. Also write log.md (#85)
    * Sensor Information Update
    
    * Log Update
    
    * LCD I2C Testing File
    
    * LCD_I2C Driver (Class)
    
    * LCD_I2C Testing File(with string format)
    
    * log Update
    
    * Remove temperature_Check.py that testing file and modify temp_Check.py that consist of only function
    
    * Remove LCD testing file
    
    * LCD_I2C Module Update
    
    * Sort files by folder
    
    * Modified import path
    
    * Added comment about code
    
    * Update Log
    
    * Finish LCD_I2C String Print Testing
    
    * Update log.md
    
    * Finish LED connect and test both LED and LCD at the same time
    
    * Modified function that lcd_Display_Write_Direction()
    
    * Modified test_7-22.py
    
    * Added function that lcd_Display_Write_Stair
    
    * Modified function of testfile
    
    * Modified test code(test_Controller.py)
    
    * Modify lcd_I2C print methods
    
    * Write log.md

--- BottomUp_Python_PI/sensor/test_Controller.py
+++ BottomUp_Python_PI/sensor/test_Controller.py
@@ -2,20 +2,16 @@ from LCD_I2C.lcd_I2C import *
 from LED.led_Driver import *
 
 lcd_Display_Clear()
+test_List = [1, 2, 4, 9]
+lcd_Display_Write_Stair(test_List)
 
 while True:
     lcd_Value = list(map(int, input("LCD 정보 입력: ").split(' ')))
     if lcd_Value[0] is -1:
         break
-    if lcd_Value.__len__() is not 4:
-        continue
-
-    led_Value = list(map(int, input("LED 정보 입력: ").split(' ')))
-    if led_Value.__len__() is not 4:
+    if lcd_Value.__len__() is not 8:
         continue
 
     lcd_Display_Write_Direction(lcd_Value)
-    light_On_LED(led_Value)
-
+    
 lcd_Display_Clear()
-light_Off()
